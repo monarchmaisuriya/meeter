a9020e36b18eb8b12a2140a505a84abcdde9763f
Monarch Maisuriya <monarchmaisuriya7600@gmail.com>
2025-04-17 15:18:35 +0530

    add util method : isNotEmpty

--- web/src/utils/helpers.ts
+++ web/src/utils/helpers.ts
@@ -6,3 +6,13 @@ export { toast } from "sonner"
 export function cn(...inputs: ClassValue[]) {
 	return twMerge(clsx(inputs))
 }
+
+export function isNotEmpty<T>(value: T | null | undefined): value is T {
+	return (
+		value !== null &&
+		value !== undefined &&
+		value !== "" &&
+		value !== false &&
+		value !== 0
+	)
+}
