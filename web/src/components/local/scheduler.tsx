35a66fcc2cee823225e7d499cc55a839d0821087
Monarch Maisuriya <monarchmaisuriya7600@gmail.com>
2025-04-17 16:09:24 +0530

    update local component : fix incorrect payloads

--- web/src/components/local/scheduler.tsx
+++ web/src/components/local/scheduler.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react"
+import React, { useState } from "react"
 import {
 	Select,
 	SelectContent,
@@ -9,29 +9,25 @@ import {
 import { Calendar } from "@/components/ui/calendar"
 import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
+import { Meeting } from "@/declarations"
+import { toast } from "sonner"
 
 interface SchedulerProps {
 	availableTimes?: string[]
-	onCreateMeeting: (type: string, meeting?: unknown) => void
-	onMeetingTypeChange: (type: string) => void
+	onCreateMeeting: (meeting: Partial<Meeting>) => Promise<unknown>
 	setDialogOpen: (open: boolean) => void
 }
 
 const Scheduler: React.FC<SchedulerProps> = ({
 	availableTimes = [],
 	onCreateMeeting,
-	onMeetingTypeChange,
 	setDialogOpen,
 }) => {
-	const [meetingType, setMeetingType] = useState<"instant" | "scheduled">(
-		"scheduled"
-	)
 	const [selectedTime, setSelectedTime] = useState<string>("09:00")
-	const [meetingLink, setMeetingLink] = useState<string>("")
 	const [selectedDate, setSelectedDate] = useState<Date | undefined>(new Date())
 	const [duration, setDuration] = useState<string>("30")
 	const [eventTitle, setEventTitle] = useState<string>("")
-	const [attendees, setAttendees] = useState<string[]>([])
+	const [attendees, setAttendees] = useState<string>("")
 
 	const defaultTimes = [
 		"09:00",
@@ -57,153 +53,129 @@ const Scheduler: React.FC<SchedulerProps> = ({
 		setSelectedTime(time)
 	}
 
-	useEffect(() => {
-		onMeetingTypeChange(meetingType)
-
-		// eslint-disable-next-line react-hooks/exhaustive-deps
-	}, [meetingType])
-
-	if (meetingType === "scheduled") {
-		return (
-			<div className="p-4">
-				<div className="my-4">
-					<label className="block text-sm font-medium text-gray-700 dark:text-gray-300 mb-1">
-						Event Attendees
-					</label>
-					<Input
-						type="text"
-						value={attendees}
-						onChange={(e) =>
-							setAttendees((prev) => {
-								return [...new Set([...prev, e.target.value])]
-							})
-						}
-						placeholder="Email addresses as comma separated values"
-					/>
-				</div>
-				<div className="flex flex-col sm:flex-row sm:space-x-8">
-					<div className="flex-1">
-						<div className="mb-4">
-							<label className="block text-sm font-medium text-gray-700 dark:text-gray-300 mb-1">
-								Event Title
-							</label>
-							<Input
-								type="text"
-								value={eventTitle}
-								onChange={(e) => setEventTitle(e.target.value)}
-								placeholder="Why create this meeting?"
-							/>
-						</div>
+	return (
+		<div className="p-4">
+			<div className="my-4">
+				<label className="block text-sm font-medium text-gray-700 dark:text-gray-300 mb-1">
+					Event Attendees
+				</label>
+				<Input
+					type="text"
+					value={attendees}
+					onChange={(e) => setAttendees(e.target.value)}
+					placeholder="Email addresses as comma separated values"
+				/>
+			</div>
+			<div className="flex flex-col sm:flex-row sm:space-x-8">
+				<div className="flex-1">
+					<div className="mb-4">
 						<label className="block text-sm font-medium text-gray-700 dark:text-gray-300 mb-1">
-							Available Dates
+							Event Title
 						</label>
-						<Calendar
-							mode="single"
-							disabled={{ before: new Date() }}
-							selected={selectedDate}
-							onSelect={setSelectedDate}
+						<Input
+							type="text"
+							value={eventTitle}
+							onChange={(e) => setEventTitle(e.target.value)}
+							placeholder="Why create this meeting?"
 						/>
 					</div>
-
-					<div className="flex-shrink-0 w-full sm:w-64">
-						<div className="mb-4">
-							<label className="block text-sm font-medium text-gray-700 dark:text-gray-300 mb-1">
-								Duration
-							</label>
-							<Select
-								value={duration}
-								onValueChange={(value) => setDuration(value)}>
-								<SelectTrigger className="w-full">
-									<SelectValue placeholder="Theme" />
-								</SelectTrigger>
-								<SelectContent className="w-[180px]">
-									<SelectItem value="30">30 minutes</SelectItem>
-									<SelectItem value="60">60 minutes</SelectItem>
-								</SelectContent>
-							</Select>
-						</div>
-						<label className="block text-sm font-medium text-gray-700 dark:text-gray-300 mb-1">
-							Available Times
-						</label>
-						<div className="grid grid-cols-2 gap-2">
-							{times.map((time) => (
-								<Button
-									key={time}
-									variant={selectedTime === time ? "default" : "outline"}
-									onClick={() => handleTimeClick(time)}
-									size={"xs"}>
-									{time}
-								</Button>
-							))}
-						</div>
-					</div>
-				</div>
-				<div className="flex justify-center flex-col gap-2">
-					<Button
-						variant="default"
-						className="cursor-pointer"
-						onClick={() => {
-							onCreateMeeting("scheduled", {
-								date: selectedDate,
-								time: selectedTime,
-								duration: duration,
-								title: eventTitle,
-							})
-							setDialogOpen(false)
-						}}>
-						Schedule Meeting
-					</Button>
-					<Button
-						variant={"link"}
-						className="cursor-pointer"
-						onClick={() => {
-							setMeetingType("instant")
-							onCreateMeeting("instant")
-							setMeetingLink("https://gemini.google.com/app")
-						}}>
-						Or Create an Instant Meeting
-					</Button>
-				</div>
-			</div>
-		)
-	} else {
-		return (
-			<div className="p-4">
-				<div className="my-4">
 					<label className="block text-sm font-medium text-gray-700 dark:text-gray-300 mb-1">
-						Meeting Link
+						Available Dates
 					</label>
-					<Input
-						type="text"
-						value={meetingLink}
-						placeholder="Use this link to join the meeting"
+					<Calendar
+						mode="single"
+						disabled={{ before: new Date() }}
+						selected={selectedDate}
+						onSelect={setSelectedDate}
 					/>
 				</div>
-				<div className="flex justify-center flex-col gap-2">
-					<Button
-						className="cursor-pointer"
-						variant="default"
-						onClick={() => {
-							if (meetingLink) {
-								window.open(meetingLink, "_blank")
-								setDialogOpen(false)
-							}
-						}}>
-						Join Meeting
-					</Button>
-					<Button
-						variant={"link"}
-						className="cursor-pointer"
-						onClick={() => {
-							setMeetingType("scheduled")
-							setMeetingLink("")
-						}}>
-						Or Schedule a Meeting
-					</Button>
+
+				<div className="flex-shrink-0 w-full sm:w-64">
+					<div className="mb-4">
+						<label className="block text-sm font-medium text-gray-700 dark:text-gray-300 mb-1">
+							Duration
+						</label>
+						<Select
+							value={duration}
+							onValueChange={(value) => setDuration(value)}>
+							<SelectTrigger className="w-full">
+								<SelectValue placeholder="Theme" />
+							</SelectTrigger>
+							<SelectContent className="w-[180px]">
+								<SelectItem value="30">30 minutes</SelectItem>
+								<SelectItem value="60">60 minutes</SelectItem>
+							</SelectContent>
+						</Select>
+					</div>
+					<label className="block text-sm font-medium text-gray-700 dark:text-gray-300 mb-1">
+						Available Times
+					</label>
+					<div className="grid grid-cols-2 gap-2">
+						{times.map((time) => (
+							<Button
+								key={time}
+								variant={selectedTime === time ? "default" : "outline"}
+								onClick={() => handleTimeClick(time)}
+								size={"xs"}>
+								{time}
+							</Button>
+						))}
+					</div>
 				</div>
 			</div>
-		)
-	}
+			<div className="flex justify-center flex-col gap-2">
+				<Button
+					variant="default"
+					className="cursor-pointer"
+					onClick={async () => {
+						if (attendees.length === 0) {
+							toast.error("Please add at least one attendee")
+						} else {
+							if (!selectedDate || !selectedTime || !duration) return
+							const [hours, minutes] = selectedTime.split(":").map(Number)
+							const startDateTime = new Date(selectedDate)
+							startDateTime.setHours(hours, minutes, 0, 0)
+							const endDateTime = new Date(startDateTime)
+							endDateTime.setMinutes(
+								endDateTime.getMinutes() + parseInt(duration, 10)
+							)
+							await onCreateMeeting({
+								startDateTime: startDateTime.toISOString(),
+								endDateTime: endDateTime.toISOString(),
+								title: eventTitle,
+								attendees: attendees.split(",").map((email) => email.trim()),
+							})
+							setDialogOpen(false)
+						}
+					}}>
+					Schedule Meeting
+				</Button>
+				<Button
+					variant={"link"}
+					className="cursor-pointer"
+					onClick={async () => {
+						const now = new Date()
+						const startDateTime = new Date(now)
+						const endDateTime = new Date(startDateTime)
+						endDateTime.setMinutes(
+							startDateTime.getMinutes() + parseInt(duration, 10)
+						)
+						await onCreateMeeting({
+							startDateTime: startDateTime.toISOString(),
+							endDateTime: endDateTime.toISOString(),
+							title: "Instant Meeting",
+							attendees:
+								attendees.length > 0
+									? attendees.split(",").map((email) => email.trim())
+									: [],
+						})
+						setDialogOpen(false)
+					}}>
+					Or Create an Instant Meeting
+				</Button>
+			</div>
+		</div>
+	)
 }
 
 export default Scheduler
