1caec7fce07b4375c87ee94863957bc2786c63dd
Monarch Maisuriya <monarchmaisuriya7600@gmail.com>
2025-04-17 11:09:46 +0530

    update oauth strategy

--- server/src/core/authentication.ts
+++ server/src/core/authentication.ts
@@ -1,8 +1,6 @@
 // For more information about this file see https://dove.feathersjs.com/guides/cli/authentication.html
 import { AuthenticationService, JWTStrategy } from "@feathersjs/authentication"
-
-import { oauth, OAuthStrategy } from "@feathersjs/authentication-oauth"
-
+import { oauth, OAuthProfile, OAuthStrategy } from "@feathersjs/authentication-oauth"
 import type { Application } from "../declarations"
 
 declare module "../declarations" {
@@ -11,11 +9,27 @@ declare module "../declarations" {
   }
 }
 
+class GoogleStrategy extends OAuthStrategy {
+  async getEntityData(profile: OAuthProfile, _existingEntity: any, _params: any) {
+    const baseData = await super.getEntityData(profile, _existingEntity, _params)
+
+    if (profile && baseData) {
+      return {
+        ...baseData,
+        email: profile.email,
+        sub: profile.sub ?? profile.id ?? ""
+      }
+    } else {
+      return {}
+    }
+  }
+}
+
 export const authentication = (app: Application) => {
   const authentication = new AuthenticationService(app)
 
   authentication.register("jwt", new JWTStrategy())
-  authentication.register("google", new OAuthStrategy())
+  authentication.register("google", new GoogleStrategy())
 
   app.use("authentication", authentication)
   app.configure(oauth())
