d0be7b9108f1d8b0ef55a2461f9e2d92db3a1a8f
Monarch Maisuriya <monarchmaisuriya7600@gmail.com>
2025-04-17 11:12:42 +0530

    add new provider : toast

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,11 +1,13 @@
 import HomePage from "@/pages/home"
 import { useTheme } from "@/hooks/useTheme"
 import { AppProvider } from "@/providers/app"
+import { Toaster } from "@/components/ui/sonner"
 
 function App() {
 	useTheme()
 	return (
 		<AppProvider>
+			<Toaster />
 			<div className="min-h-screen bg-background text-foreground p-4">
 				<HomePage />
 			</div>
