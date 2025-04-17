86093fc8db90d477edb4b8f16e152ee87fab9cdd
Monarch Maisuriya <monarchmaisuriya7600@gmail.com>
2025-04-18 00:48:24 +0530

    add NODE_ENV log

--- server/src/index.ts
+++ server/src/index.ts
@@ -3,9 +3,10 @@ import { logger } from "./utils/logger"
 
 const port = app.get("port") ?? 3030
 const host = app.get("host") ?? "localhost"
+const env = process.env.NODE_ENV || ""
 
 process.on("unhandledRejection", reason => logger.error("Unhandled Rejection %O", reason))
 
 app.listen(port).then(() => {
-  logger.info(`Feathers app listening on http://${host}:${port}`)
+  logger.info(`${env}: Feathers app listening on http://${host}:${port}`)
 })
